410663380b663466e459687662374ddfe71a784f
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-06 20:50:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,9 +3,9 @@ def roll_call_dwarves(names)
   #(idx + 1).to_s + ". " + name}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+# def summon_captain_planet# code an argument here
+#   # Your code here
+# end
 
 # def long_planeteer_calls# code an argument here
 #   # Your code here
