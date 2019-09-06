24094e5239edc3f17776b0276290dfac0e043834
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-06 20:39:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,15 +2,15 @@ def roll_call_dwarves(names)
   names.each_with_index {|name| puts name}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+# def summon_captain_planet# code an argument here
+#   # Your code here
+# end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+# def long_planeteer_calls# code an argument here
+#   # Your code here
+# end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+# def find_the_cheese# code an argument here
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+# end
