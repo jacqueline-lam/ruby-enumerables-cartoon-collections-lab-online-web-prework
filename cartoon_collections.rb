afcac33eaa8ddfc071d870f94219a25db91dfa91
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-06 22:58:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(food)
-  if food
+  if food.include?("cheddar")
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
