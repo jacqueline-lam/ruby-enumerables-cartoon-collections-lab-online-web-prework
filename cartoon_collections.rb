600a0ff5373506aaa1426a030ee53c2f20a505a6
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-06 22:57:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ def long_planeteer_calls(calls)
   calls.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(food)
+  food.each 
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
