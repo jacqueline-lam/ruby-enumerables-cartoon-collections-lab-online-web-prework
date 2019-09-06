499df427cd258b512e99709eecfcf4020ac5ccc4
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-06 21:23:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,9 +6,9 @@ def summon_captain_planet(calls)
   calls.map { |call| "#{call.capitalize}!" } 
 end
 
-# def long_planeteer_calls# code an argument here
-#   # Your code here
-# end
+def long_planeteer_calls
+  # Your code here
+end
 
 # def find_the_cheese# code an argument here
 #   # the array below is here to help
